fd813190d837acbc55245572e2b4f73c3bb5dc49
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-03-13 01:25:59 +0300

    added customer model

---
+++ app/customer.py
@@ -0,0 +1,22 @@
+#!/home/k/.pyenv/shims/python
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from.base import Base
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    phone = Column(String, unique=True, nullable=False)
+
+    orders = relationship("Order",  back_populates="customer")
+
+
+    def __repr__(self):
+        return f"<Customer {self.name} - {self.phone}>"
+    
+    
