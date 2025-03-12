a6db4b0a6d7f48fda0c6cf60d62d49f561e3037a
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-03-13 00:35:35 +0300

    base model

---
+++ app/base.py
@@ -0,0 +1,13 @@
+#!/home/k/.pyenv/shims/python
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+my_engine = create_engine("sqlite:///restaurant.db" , echo = True)
+Session = sessionmaker(bind = my_engine)
+session = Session()
+
+Base = declarative_base()
+
